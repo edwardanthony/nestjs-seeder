c97ac83182e20b0e85abecd41b4fcb0361b4970f
Edward Anthony <edwardanthony@fastmail.com>
2020-10-25 05:10:35 +0700

    Update the sample to show the new feature

--- sample/src/schemas/user.schema.ts
+++ sample/src/schemas/user.schema.ts
@@ -4,10 +4,15 @@ import { Factory } from 'nestjs-seeder';
 
 @Schema()
 export class User extends Document {
-  // @Factory will automatically inject faker to the function you that you pass.
-  @Factory(faker => ({
-    first: faker.name.firstName(),
-    last: faker.name.lastName(),
+  // @Factory will automatically inject faker to the function that you pass.
+  @Factory(faker => faker.random.arrayElement(['male', 'female']))
+  @Prop({ required: true })
+  gender: string;
+
+  // You could get the previously generated value using the passed context.
+  @Factory((faker, ctx) => ({
+    first: faker.name.firstName(ctx.gender === 'male' ? 0 : 1),
+    last: faker.name.lastName(ctx.gender === 'male' ? 0 : 1),
   }))
   @Prop(
     raw({
@@ -27,11 +32,13 @@ export class User extends Document {
   age: number;
 
   // You could also use static value.
-  @Factory('male')
+  @Factory('admin')
   @Prop({ required: true })
-  gender: string;
+  role: string; 
 
-  @Factory(faker => faker.address.streetAddress())
+  // If you pass predefined values to the `generate` function, you will be 
+  // able to access it in the context.
+  @Factory((faker, ctx) => `${faker.address.streetAddress()} ${ctx.zipCode}`)
   @Prop({ required: true })
   address: string;
 }
--- sample/src/seeders/users.seeder.ts
+++ sample/src/seeders/users.seeder.ts
@@ -10,7 +10,9 @@ export class UsersSeeder implements Seeder {
 
   async seed(): Promise<any> {
     // Generate 10 users.
-    const users = DataFactory.createForClass(User).generate(10);
+    // Notice that we can optionally pass values that will be accessible in
+    // the context that's passed to the @Factory decorator.
+    const users = DataFactory.createForClass(User).generate(10, { zipCode: '10153' });
 
     // Insert into the database.
     return this.user.insertMany(users);
