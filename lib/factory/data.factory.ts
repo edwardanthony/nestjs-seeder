e0d96f9010fd6a7c9f0e7b55255d8df5761e855c
Jeremiah <jeremiahkoeiman1@gmail.com>
2023-12-28 23:51:50 +0100

    allow usage of predefined values in generate method

--- lib/factory/data.factory.ts
+++ lib/factory/data.factory.ts
@@ -1,8 +1,8 @@
-import { Type } from '@nestjs/common';
-import { Factory, PropertyMetadata } from '../interfaces';
-import { FactoryValue } from '../decorators/factory.decorator';
-import { FactoryMetadataStorage } from '../storages/factory.metadata.storage';
-import { faker } from '@faker-js/faker';
+import { Type } from "@nestjs/common";
+import { Factory, PropertyMetadata } from "../interfaces";
+import { FactoryValue } from "../decorators/factory.decorator";
+import { FactoryMetadataStorage } from "../storages/factory.metadata.storage";
+import { faker } from "@faker-js/faker";
 
 export class DataFactory {
   static createForClass(target: Type<unknown>): Factory {
@@ -16,7 +16,10 @@ export class DataFactory {
       FactoryMetadataStorage.getPropertyMetadatasByTarget(target);
 
     return {
-      generate: (count: number, values: Record<string, any> = {}): Record<string, FactoryValue>[] => {
+      generate: (
+        count: number,
+        values: Record<string, any> = {}
+      ): Record<string, FactoryValue>[] => {
         const ret = Array<Record<string, FactoryValue>>();
         for (let i = 0; i < count; i++) {
           ret.push(this.generate(properties, values));
@@ -28,13 +31,15 @@ export class DataFactory {
 
   private static generate(
     properties: PropertyMetadata[],
-    values: Record<string, any>,
+    values: Record<string, any>
   ): Record<string, FactoryValue> {
     const ctx = { ...values };
     return properties.reduce(
       (r, p) => ({
-        [p.propertyKey]: ctx[p.propertyKey] = typeof p.arg === 'function' ? p.arg(faker, ctx) : p.arg,
+        [p.propertyKey]: (ctx[p.propertyKey] =
+          typeof p.arg === "function" ? p.arg(faker, ctx) : p.arg),
         ...r,
+        ...ctx,
       }),
       {}
     );
